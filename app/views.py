0b9a155bd02a9a2a91f4f01e957ea38edd980729
Volodymyr Lyubinets <vlyubin@gmail.com>
2019-10-02 22:11:37 -0700

    more boilerplate

--- app/views.py
+++ app/views.py
@@ -4,9 +4,30 @@ from app import app
 import datetime
 import os, json
 
+@app.route('/')
+def home():
+  return render_template('index.html')
+
+@app.route('/logout')
+def logout():
+  # TODO: Remove login cookie
+  return redirect('/')
+
 @app.route('/login')
 def login():
   return render_template('login.html')
+
+@app.route('/self-diagnosis')
+def sd():
+  return render_template('self_diagnose.html')
+
+@app.route('/staying-healthy')
+def sh():
+  return render_template('stay_healthy.html')
+
+@app.route('/my-info')
+def claims():
+  return render_template('claims.html')
 
 @app.route('/base')
 def base():
